186fc7066f742f1c2c6e9a2ba64b857538a46861
Milan Kumar Giri <36377952+MILANGIRI5@users.noreply.github.com>
2025-08-06 00:45:50 +0530

    Update TestService.java

--- src/main/java/com/example/git_test_project/service/TestService.java
+++ src/main/java/com/example/git_test_project/service/TestService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 public class TestService {
 
     public String getTestValue() {
+        String a = "test";
         return "Application is working as expected............done";
     }
 }
